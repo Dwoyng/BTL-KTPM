8461157c88206bc5acd60d153af41fb09c841de0
duongthai18 <thaiduong180804@gmail.com>
2025-05-28 02:00:39 +0700

    Ignore .vs folder

---
+++ QLCN20250505-admin/WinApp/Controllers/DonViHanhChinhController.cs
@@ -0,0 +1,67 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApp.Controllers
+{
+    internal class DonViHanhChinhController : DataController<DonViHanhChinh>
+    {
+        
+        public object ThongKeHuyen()
+        {
+            // Lấy các ID cấp Huyện, Thành phố (trực thuộc tỉnh)
+            var capDoHuyenIds = Provider.GetTable<CapDoHanhChinh>()
+                .ToList<CapDoHanhChinh>(null, "TenCapDo IN (N'Huyện', N'Thành phố')")
+                .Select(x => x.ID)
+                .ToList();
+
+            if (capDoHuyenIds.Count == 0)
+                return View(new List<DonViHanhChinh>());
+
+            // Sinh chuỗi IDs cho SQL filter
+            string ids = string.Join(",", capDoHuyenIds);
+
+            var dsHuyen = Provider.GetTable<DonViHanhChinh>()
+                .ToList<DonViHanhChinh>(null, $"CapDoHanhChinhID IN ({ids})");
+
+            return View(dsHuyen);
+        }
+        public object ThongKePhuong()
+        {
+            var capDoPhuongIds = Provider.GetTable<CapDoHanhChinh>()
+                .ToList<CapDoHanhChinh>(null, "TenCapDo IN (N'Phường', N'Thị trấn')")
+                .Select(x => x.ID)
+                .ToList();
+
+            if (capDoPhuongIds.Count == 0)
+                return View(new List<DonViHanhChinh>());
+
+            string ids = string.Join(",", capDoPhuongIds);
+
+            var dsPhuong = Provider.GetTable<DonViHanhChinh>()
+                .ToList<DonViHanhChinh>(null, $"CapDoHanhChinhID IN ({ids})");
+
+            return View(dsPhuong);
+        }
+        public object ThongKeXa()
+        {
+            var capDoXaIds = Provider.GetTable<CapDoHanhChinh>()
+                .ToList<CapDoHanhChinh>(null, "TenCapDo = N'Xã'")
+                .Select(x => x.ID)
+                .ToList();
+
+            if (capDoXaIds.Count == 0)
+                return View(new List<DonViHanhChinh>());
+
+            string ids = string.Join(",", capDoXaIds);
+
+            var dsXa = Provider.GetTable<DonViHanhChinh>()
+                .ToList<DonViHanhChinh>(null, $"CapDoHanhChinhID IN ({ids})");
+
+            return View(dsXa);
+        }
+    }
+}
---
+++ QLCN20250505-admin/WinApp/Views/DonViHanhChinh/DonViHanhChinhManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace WinApp.Views.DonViHanhChinh
+{
+    using Vst.Controls;
+    using Models;
+    class Index : BaseView<DataListViewLayout>
+    {
+        protected override void RenderCore(ViewContext context)
+        {
+            base.RenderCore(context);
+            context.Title = "Danh sách đơn vị hành chính";
+            context.TableColumns = new object[] {
+                new TableColumn { Name = "TenDonVi", Caption = "Tên Đơn Vị", Width = 200, },
+                new TableColumn { Name = "CapDoHanhChinh", Caption = "Cấp", Width = 120, },
+                new TableColumn { Name = "TrucThuoc", Caption = "Trực Thuộc", Width = 200, },
+            };
+        }
+    }
+    class ThongKeHuyen : BaseView<DataListViewLayout>
+    {
+        protected override void RenderCore(ViewContext context)
+        {
+            context.Title = "Danh sách Huyện/Thành phố";
+            context.TableColumns = new object[] {
+                new TableColumn { Name = "TenDonVi", Caption = "Tên Huyện/Thành phố", Width = 200, },
+                new TableColumn { Name = "CapDoHanhChinh", Caption = "Cấp", Width = 120, },
+                new TableColumn { Name = "TrucThuoc", Caption = "Trực Thuộc", Width = 200, },
+            };
+        }
+    }
+    class ThongKePhuong : BaseView<DataListViewLayout>
+    {
+        protected override void RenderCore(ViewContext context)
+        {
+            context.Title = "Danh sách Phường/Thị trấn";
+            context.TableColumns = new object[] {
+                new TableColumn { Name = "TenDonVi", Caption = "Tên Phường/Thị trấn", Width = 200, },
+                new TableColumn { Name = "CapDoHanhChinh", Caption = "Cấp", Width = 120, },
+                new TableColumn { Name = "TrucThuoc", Caption = "Trực Thuộc", Width = 200, },
+            };
+        }
+    }
+    class ThongKeXa : BaseView<DataListViewLayout>
+    {
+        protected override void RenderCore(ViewContext context)
+        {
+            context.Title = "Danh sách Xã";
+            context.TableColumns = new object[] {
+                new TableColumn { Name = "TenDonVi", Caption = "Tên Xã", Width = 200, },
+                new TableColumn { Name = "CapDoHanhChinh", Caption = "Cấp", Width = 120, },
+                new TableColumn { Name = "TrucThuoc", Caption = "Trực Thuộc", Width = 200, },
+            };
+        }
+    }
+    class Add : EditView
+    {
+        protected override void RenderCore(ViewContext context)
+        {
+            base.RenderCore(context);
+            context.Title = "Thông Tin Đơn Vị Hành Chính";
+            context.Editors = new object[] {
+                new EditorInfo { Name = "TenDonVi", Caption = "Tên đơn vị", Layout = 12,   },
+                new EditorInfo { Name = "CapDoHanhChinhID", Caption = "Cấp", Layout = 12,
+                    Type = "select", ValueName = "ID", DisplayName = "TenCapDo", Options = Provider.Select<CapDoHanhChinh>(), },
+                new EditorInfo { Name = "TrucThuocID", Caption = "Trực thuộc", Layout = 12,
+                    Type = "select", ValueName = "ID", DisplayName = "TenDonVi", Options = Provider.Select<DonViHanhChinh>(), },
+            };
+        }
+    }
+    class Edit : Add
+    {
+        protected override void OnReady()
+        {
+            ShowDeleteAction("TenDonVi"); // Hiển thị tên đơn vị khi xác nhận xóa
+            Find("CapDoHanhChinhID", c => c.IsEnabled = false); // Không cho sửa cấp hành chính khi edit
+        }
+    }
+}
