1fac2a29d67afdbb21ce767326bdb0a8b1f8e35f
duongthai18 <thaiduong180804@gmail.com>
2025-06-02 20:53:21 +0700

    FINAL

---
+++ QLCN20250505-admin/WinApp/Models/TaiKhoan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    partial class TaiKhoan
+    {
+    public string Quyen 
+        { 
+        get
+            {
+                return Provider.GetTable<Quyen>().Find<Quyen>(Quyen).Ten;
+            }
+        }
+    }
+}
